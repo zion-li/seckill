70422860ddd14d554c5660886bf48e3b38df9693
zion.li <zion.li@baifendian.com>
2018-07-19 09:58:39 +0800

    配置数据库与mybatis

---
+++ src/main/java/com/myself/seckill/config/DatasourceConfig.java
@@ -0,0 +1,137 @@
+package com.myself.seckill.config;
+
+
+import com.alibaba.druid.pool.DruidDataSource;
+import com.alibaba.druid.support.http.StatViewServlet;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+import javax.sql.DataSource;
+import java.sql.SQLException;
+
+/**
+ * Druid配置
+ *
+ * @author zhanyong
+ * @Date 2017-12-19 21:11:57
+ */
+@Configuration
+public class DatasourceConfig {
+
+    private Logger logger = LoggerFactory.getLogger(DatasourceConfig.class);
+
+    @Value("${spring.datasource.url}")
+    private String dbUrl;
+
+    @Value("${spring.datasource.type}")
+    private String dbType;
+
+    @Value("${spring.datasource.username}")
+    private String username;
+
+    @Value("${spring.datasource.password}")
+    private String password;
+
+    @Value("${spring.datasource.driver-class-name}")
+    private String driverClassName;
+
+    @Value("${spring.datasource.initialSize}")
+    private int initialSize;
+
+    @Value("${spring.datasource.minIdle}")
+    private int minIdle;
+
+    @Value("${spring.datasource.maxActive}")
+    private int maxActive;
+
+    @Value("${spring.datasource.maxWait}")
+    private int maxWait;
+
+    @Value("${spring.datasource.timeBetweenEvictionRunsMillis}")
+    private int timeBetweenEvictionRunsMillis;
+
+    @Value("${spring.datasource.minEvictableIdleTimeMillis}")
+    private int minEvictableIdleTimeMillis;
+
+    @Value("${spring.datasource.validationQuery}")
+    private String validationQuery;
+
+    @Value("${spring.datasource.testWhileIdle}")
+    private boolean testWhileIdle;
+
+    @Value("${spring.datasource.testOnBorrow}")
+    private boolean testOnBorrow;
+
+    @Value("${spring.datasource.testOnReturn}")
+    private boolean testOnReturn;
+
+    @Value("${spring.datasource.poolPreparedStatements}")
+    private boolean poolPreparedStatements;
+
+    @Value("${spring.datasource.filters}")
+    private String filters;
+
+    @Value("${spring.datasource.connectionProperties}")
+    private String connectionProperties;
+
+    @Value("${spring.datasource.useGlobalDataSourceStat}")
+    private boolean useGlobalDataSourceStat;
+
+    @Value("${spring.datasource.druidLoginName}")
+    private String druidLoginName;
+
+    @Value("${spring.datasource.druidPassword}")
+    private String druidPassword;
+
+    @Bean(name = "dataSource", destroyMethod = "close", initMethod = "init")
+    @Primary
+    public DataSource dataSource() {
+        DruidDataSource datasource = new DruidDataSource();
+        try {
+            datasource.setUrl(this.dbUrl);
+            datasource.setUsername(username);
+            datasource.setPassword(password);
+            datasource.setDriverClassName(driverClassName);
+            datasource.setInitialSize(initialSize);
+            datasource.setMinIdle(minIdle);
+            datasource.setMaxActive(maxActive);
+            datasource.setMaxWait(maxWait);
+            datasource.setTimeBetweenEvictionRunsMillis(timeBetweenEvictionRunsMillis);
+            datasource.setMinEvictableIdleTimeMillis(minEvictableIdleTimeMillis);
+            datasource.setValidationQuery(validationQuery);
+            datasource.setTestWhileIdle(testWhileIdle);
+            datasource.setTestOnBorrow(testOnBorrow);
+            datasource.setTestOnReturn(testOnReturn);
+            datasource.setPoolPreparedStatements(poolPreparedStatements);
+            datasource.setFilters(filters);
+            datasource.setConnectionProperties(connectionProperties);
+            datasource.setUseGlobalDataSourceStat(useGlobalDataSourceStat);
+        } catch (SQLException e) {
+            logger.error("druid configuration initialization filter", e);
+        }
+        return datasource;
+    }
+
+    /**
+     * druid 监控访问设置
+     *
+     * @return
+     */
+    @Bean
+    public ServletRegistrationBean druidServlet() {
+        ServletRegistrationBean reg = new ServletRegistrationBean();
+        reg.setServlet(new StatViewServlet());
+        reg.addUrlMappings("/druid/*");  //url 匹配
+        reg.addInitParameter("allow", "127.0.0.1"); // IP白名单 (没有配置或者为空，则允许所有访问)
+        reg.addInitParameter("deny", "192.168.11.111"); //IP黑名单 (存在共同时，deny优先于allow)
+        reg.addInitParameter("loginUsername", this.druidLoginName);//登录名
+        reg.addInitParameter("loginPassword", this.druidPassword);//登录密码
+        reg.addInitParameter("resetEnable", "false"); // 禁用HTML页面上的“Reset All”功能
+        return reg;
+    }
+}
---
+++ src/main/java/com/myself/seckill/config/MybatisConfiguration.java
@@ -0,0 +1,97 @@
+package com.myself.seckill.config;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.apache.ibatis.plugin.Interceptor;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.AutoConfigureAfter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.core.io.DefaultResourceLoader;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.transaction.annotation.TransactionManagementConfigurer;
+
+import javax.sql.DataSource;
+import java.io.IOException;
+import java.util.Properties;
+
+/**
+ * Mybatis配置
+ *
+ * @author zhanyong
+ * @Date 2017-12-19 21:11:57
+ */
+@Configuration
+@AutoConfigureAfter(DatasourceConfig.class)
+@ConfigurationProperties
+@EnableTransactionManagement
+public class MybatisConfiguration implements TransactionManagementConfigurer {
+
+    private static Log logger = LogFactory.getLog(MybatisConfiguration.class);
+
+    //  配置类型别名
+    @Value("${mybatis.typeAliasesPackage}")
+    private String typeAliasesPackage;
+
+    @Value("${mybatis.mapperLocations}")
+    private String mapperLocations;
+
+    //  加载全局的配置文件
+    @Value("${mybatis.configLocation}")
+    private String configLocation;
+
+    @Autowired
+    @Qualifier("dataSource")
+    private DataSource dataSource;
+
+    // 提供SqlSeesion
+    @Bean(name = "sqlSessionFactory")
+    @Primary
+    public SqlSessionFactory sqlSessionFactory() {
+        try {
+            SqlSessionFactoryBean sessionFactoryBean = new SqlSessionFactoryBean();
+            sessionFactoryBean.setDataSource(dataSource);
+
+            // 读取配置
+            sessionFactoryBean.setTypeAliasesPackage(typeAliasesPackage);
+
+            //设置mapper.xml文件所在位置
+            Resource[] resources = new PathMatchingResourcePatternResolver()
+                    .getResources(mapperLocations);
+            sessionFactoryBean.setMapperLocations(resources);
+            //设置mybatis-config.xml配置文件位置
+            sessionFactoryBean.setConfigLocation(new DefaultResourceLoader().getResource(configLocation));
+
+            return sessionFactoryBean.getObject();
+        } catch (IOException e) {
+            logger.error("mybatis resolver mapper*xml is error", e);
+            return null;
+        } catch (Exception e) {
+            logger.error("mybatis sqlSessionFactoryBean create error", e);
+            return null;
+        }
+    }
+
+    @Bean
+    public SqlSessionTemplate sqlSessionTemplate(SqlSessionFactory sqlSessionFactory) {
+        return new SqlSessionTemplate(sqlSessionFactory);
+    }
+
+    //事务管理
+    @Bean
+    @Override
+    public PlatformTransactionManager annotationDrivenTransactionManager() {
+        return new DataSourceTransactionManager(dataSource);
+    }
+}
